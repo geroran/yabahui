a3acb66b5b34b5e39fe03d63f51ea864af37fb3f
geroran <geroran@163.com>
2025-12-05 16:59:12 +0800

    游戏3开场

--- pages/2-3/index.js
+++ pages/2-3/index.js
@@ -1,8 +1,27 @@
 Page({
   data: {
-
+    gameState: 'opening', // opening, puzzle, rhythm, resource, ending
   },
-  onLoad(options) {
 
+  onLoad(options) {
+    // 预加载音效等资源
+  },
+
+  // 点击屏幕处理
+  handleTap() {
+    if (this.data.gameState === 'opening') {
+      this.startGame();
+    }
+  },
+
+  // 开始游戏，进入下一阶段
+  startGame() {
+    wx.showToast({
+      title: '即将进入解谜阶段',
+      icon: 'none'
+    });
+
+    // TODO: 切换到解谜阶段
+    // this.setData({ gameState: 'puzzle' });
   }
 })
